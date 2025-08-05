678dc739b2da41c75bde35e35f08f68cce0460b8
Aditya7Chaudhary <adityachaudhary.eg@gmail.com>
2025-08-05 21:48:14 +0530

    Renaming the files names

--- Tip_Calculator.py
+++
@@ -1,9 +0,0 @@
-print("Welcome to the tip calculator!")
-bill = int(input("What was the total bill ? : "))
-tip = int(input("How much tip will you like to give ? : "))
-people = int(input("How many people are splitting the bill ? : "))
-
-total_bill = bill*(1+tip/100)
-net_bill = round(total_bill/people, 3)
-
-print(f"Each person should pay : ${net_bill}")
