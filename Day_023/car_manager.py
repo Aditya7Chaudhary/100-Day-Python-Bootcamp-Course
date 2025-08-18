da61e4ee3bfec1b65cfc6bacfe6c93bc5e3141e1
Aditya7Chaudhary <adityachaudhary.eg@gmail.com>
2025-08-18 17:48:46 +0530

    Day 23 completed

--- Day_023/car_manager.py
+++ Day_023/car_manager.py
@@ -1,7 +1,27 @@
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
-
+from turtle import Turtle
+import random
 
 class CarManager:
-    pass
+    def __init__(self):
+        self.car = Turtle()
+        car = self.car
+        car.penup()
+        car.shape('square')
+        car.setheading(180)
+        car.color(random.choice(COLORS))
+        car.speed(6)
+        car.shapesize(1,2)
+    
+    def move(self):
+        self.car.forward(STARTING_MOVE_DISTANCE)
+    
+    def car_loop(self):
+        x,y = self.car.pos()
+        if x <= -300:
+            self.car.goto((300,y))
+
+    def deleting_car(self):
+        self.car.color('white')
