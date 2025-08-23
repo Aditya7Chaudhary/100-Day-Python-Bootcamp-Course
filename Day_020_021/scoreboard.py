4fb5ffc71a8a71fc60b41c00106fc6f9db4816cb
Aditya7Chaudhary <adityachaudhary.eg@gmail.com>
2025-08-23 23:51:14 +0530

    Day 24 completed

--- Day_020_021/scoreboard.py
+++ Day_020_021/scoreboard.py
@@ -3,12 +3,22 @@ from turtle import Turtle
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
+        self.highscore  = 0
+        self.score = 0
         self.hideturtle()
         self.color("white")
         self.penup()
         self.goto((0,230))
-        self.write(f"Score is : {0}",align="center",font=("Arial",10,"bold"))
+        self.write(f"Score is : {self.score} Highscore is : {self.highscore}",align="center",font=("Arial",10,"bold"))
     
     def update_score(self,score):
         self.clear()
-        self.write(f"Score is : {score}",align="center",font=("Arial",10,"bold"))
+        self.write(f"Score is : {score} Highscore is : {self.highscore}",align="center",font=("Arial",10,"bold"))
+    
+    def highscore_check(self,score):
+        self.clear()
+        if score > self.highscore:
+            self.highscore = score
+            self.score = 0
+        self.write(f"Score is : {self.score} Highscore is : {self.highscore}",align="center",font=("Arial",10,"bold"))
+        return self.score
