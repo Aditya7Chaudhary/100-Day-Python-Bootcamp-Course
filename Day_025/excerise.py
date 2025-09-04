4cee80a25367045531de0f5389287f487785bea9
Aditya7Chaudhary <adityachaudhary.eg@gmail.com>
2025-09-04 18:04:27 +0530

    Day 25

--- Day_025/excerise.py
+++ Day_025/excerise.py
@@ -1,7 +1,18 @@
 import csv
 
+"""
 with open(r"100-Day-Python-Bootcamp-Course\Day_025\weather_data.csv","r") as file:
     data = csv.reader(file)
     temperature = []
     for row in data:
-        print(row)
+        if row[1] == 'temp':
+            continue
+        temp = int(row[1])
+        temperature.append(temp)
+    print(temperature)
+"""
+
+import pandas
+
+data = pandas.read_csv(r"100-Day-Python-Bootcamp-Course\Day_025\weather_data.csv")
+print(data)
