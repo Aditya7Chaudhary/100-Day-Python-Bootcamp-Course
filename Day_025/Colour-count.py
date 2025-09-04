b99fa2d5293bfb7f77d1b4fe108abab89ccffbc4
Aditya7Chaudhary <adityachaudhary.eg@gmail.com>
2025-09-04 21:49:34 +0530

    Day 25

---
+++ Day_025/Colour-count.py
@@ -0,0 +1,4 @@
+,color,count
+0,Gray,2474
+1,Cinnamon,393
+2,Black,104
---
+++ Day_025/excerise2.py
@@ -0,0 +1,25 @@
+import pandas
+
+data = pandas.read_csv(r"100-Day-Python-Bootcamp-Course\Day_025\2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+colours = data["Primary Fur Color"]
+
+l = {}
+for i in colours:
+    if i not in l.keys():
+        l[i] = 1
+    l[i] += 1
+
+dict = {}
+color = []
+count = []
+for i in l:
+    color.append(i)
+    count.append(l[i])
+color.pop(0)
+count.pop(0)
+dict["color"] = color
+dict["count"] = count
+
+df = pandas.DataFrame(dict)
+print(df)
+df.to_csv("100-Day-Python-Bootcamp-Course\Day_025\Colour-count.py")
