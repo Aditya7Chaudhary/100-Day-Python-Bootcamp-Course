1bbca62dde216545bff3d4735688341a936919dd
Aditya7Chaudhary <adityachaudhary.eg@gmail.com>
2025-09-11 20:11:05 +0530

    Day 27

--- Day_027/practice.py
+++ Day_027/practice.py
@@ -7,4 +7,10 @@ window.title("My first GUI project!!")
 label = tkinter.Label(text="Hello world",font=("Arial",20,"bold"))
 label.pack()
 
+def button_click():
+    label.config(text="Clicked!")
+
+button = tkinter.Button(text="Click me",command=button_click)
+button.pack()
+
 window.mainloop()
