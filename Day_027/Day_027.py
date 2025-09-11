c9f17a8374b4c7c3a93e11bb31c4b486b7c0a1c1
Aditya7Chaudhary <adityachaudhary.eg@gmail.com>
2025-09-11 21:44:35 +0530

    Day 27 Competed

--- Day_027/Day_027.py
+++ Day_027/Day_027.py
@@ -7,12 +7,31 @@ class MileToKilometer:
         self.window.minsize(200,200)
         self.window.config(padx=50,pady=50)
 
-        self.label = tkinter.Label(text="Mile :")
-        self.label.grid(column=0,row=0)
+        self.label_mile = tkinter.Label(text="Mile :")
+        self.label_mile.config(padx=10)
+        self.label_mile.grid(column=0,row=0)
 
         self.input_mile = tkinter.Entry()
         self.input_mile.grid(columnspan=2,column=1,row=0)
 
+        self.button = tkinter.Button(text="Convert",command=self.convert)
+        self.button.grid(column=1,row=1)
+        self.button.config(pady=10)
+
+        self.label_kilo = tkinter.Label(text="Kilo :")
+        self.label_kilo.config(padx=10)
+        self.label_kilo.grid(column=0,row=2)
+
+        self.input_kilo = tkinter.Entry()
+        self.input_kilo.grid(columnspan=2,column=1,row=2)
+
         self.window.mainloop()
+    
+    def convert(self):
+        mile = self.input_mile.get()
+        kilo  = int(mile)*1.609344
+        self.input_kilo.delete(0,tkinter.END)
+        self.input_kilo.insert(0,string=str(kilo))
+        
 
 converter = MileToKilometer()
