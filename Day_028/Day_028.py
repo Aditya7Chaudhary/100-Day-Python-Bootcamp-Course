cccf4953b6ff1b47715250519dff67d9ebd96d79
Aditya7Chaudhary <adityachaudhary.eg@gmail.com>
2025-09-13 10:25:44 +0530

    Day 28

--- Day_028/Day_028.py
+++ Day_028/Day_028.py
@@ -16,13 +16,21 @@ window = tkinter.Tk()
 window.title("Pomodora")
 window.config(padx=50,pady=50,bg=YELLOW)
 
+label = tkinter.Label(text="Time on the clock",font=(FONT_NAME,30,"normal"),bg=YELLOW,fg=GREEN)
+
 canvas = tkinter.Canvas()
 tomato_image = tkinter.PhotoImage(file=r"100-Day-Python-Bootcamp-Course\Day_028\tomato.png")
 canvas.config(bg=YELLOW,highlightthickness=0)
-canvas.create_image(190,140,image=tomato_image)
-canvas.create_text(190,170,text="00:00",font=(FONT_NAME,30,"bold"),fill="white")
-canvas.pack()
+canvas.create_image(190,110,image=tomato_image)
+canvas.create_text(190,140,text="00:00",font=(FONT_NAME,30,"bold"),fill="white")
 
+start_button = tkinter.Button(text="Start")
+reset_button = tkinter.Button(text="Reset")
+
+label.grid(column=0,row=0,columnspan=3)
+canvas.grid(column=0,row=1,columnspan=3)
+start_button.grid(column=0,row=2)
+reset_button.grid(column=2,row=2)
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
