ed16928f27947e2d7e5abb2b88b81e87776d0032
Aditya7Chaudhary <adityachaudhary.eg@gmail.com>
2025-09-13 09:43:42 +0530

    Day 28

---
+++ Day_028/Day_028.py
@@ -0,0 +1,25 @@
+
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+
+import tkinter
+
+window = tkinter.Tk()
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+window.mainloop()
