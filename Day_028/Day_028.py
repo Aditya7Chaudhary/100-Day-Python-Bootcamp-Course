7b181f5ae88dcc37768ab6a4506ba47137c767f9
Aditya7Chaudhary <adityachaudhary.eg@gmail.com>
2025-09-13 10:01:31 +0530

    Day 28

--- Day_028/Day_028.py
+++ Day_028/Day_028.py
@@ -13,6 +13,16 @@ LONG_BREAK_MIN = 20
 import tkinter
 
 window = tkinter.Tk()
+window.title("Pomodora")
+window.config(padx=50,pady=50,bg=YELLOW)
+
+canvas = tkinter.Canvas()
+tomato_image = tkinter.PhotoImage(file=r"100-Day-Python-Bootcamp-Course\Day_028\tomato.png")
+canvas.config(bg=YELLOW,highlightthickness=0)
+canvas.create_image(190,140,image=tomato_image)
+canvas.create_text(190,170,text="00:00",font=(FONT_NAME,30,"bold"),fill="white")
+canvas.pack()
+
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
