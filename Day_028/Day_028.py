b8a7dbcc0517e274faa750b866dcc83a392ebb4d
Aditya7Chaudhary <adityachaudhary.eg@gmail.com>
2025-09-18 20:57:46 +0530

    Day 28

--- Day_028/Day_028.py
+++ Day_028/Day_028.py
@@ -5,7 +5,7 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
+WORK_MIN = 1
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
@@ -86,6 +86,15 @@ def reset_timer():
     text_id = canvas.create_text(190, 140, text=f"{current_time//60:02d}:{current_time%60:02d}", 
                                 font=(FONT_NAME, 30, "bold"), fill="white")
     start_button.config(text="Start")
+
+def break_time():
+    global current_time
+    if current_time == 0:
+        current_time = SHORT_BREAK_MIN*60
+        canvas.delete(text_id)
+        text_id = canvas.create_text(190, 140, text=f"{current_time//60:02d}:{current_time%60:02d}", 
+                                    font=(FONT_NAME, 30, "bold"), fill="white")
+
 
 # Fixed button commands - no parentheses and no parameters in the command reference
 start_button = tkinter.Button(text="Start", command=clicking_of_start_button)
